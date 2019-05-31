48085917547d06a104ab3f2e72aaa7ad534a13ae
JLAvantro <44925290+JLAvantro@users.noreply.github.com>
2019-05-31 15:17:08 +0200

    add AllTests

---
+++ JPU-BlankProject/model/src/test/java/model/element/AllTests.java
@@ -0,0 +1,11 @@
+package model.element;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PermeabilityTest.class })
+public class AllTests {
+
+}
