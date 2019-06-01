cf959ce2351c68d2c3044825d43d01e660ee4205
leo <leoolivier59@gmail.com>
2019-06-01 18:52:14 +0200

    Update model

---
+++ JPU-BlankProject/model/src/test/java/model/DAOMapTest.java
@@ -0,0 +1,59 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DAOMapTest extends DAOHelloWorld {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDAOHelloWorld() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateLevel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteLevel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateLevel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindString() {
+		fail("Not yet implemented");
+	}
+
+}
