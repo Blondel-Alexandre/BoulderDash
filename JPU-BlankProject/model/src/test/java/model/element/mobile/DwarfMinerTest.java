beac97cf40952b0f8ccd70a9b65d81946ecedcd3
ThePikPik <44901108+ThePikPik@users.noreply.github.com>
2019-06-03 19:11:50 +0200

    Update DwarfMinerTest

--- JPU-BlankProject/model/src/test/java/model/element/mobile/DwarfMinerTest.java
+++ JPU-BlankProject/model/src/test/java/model/element/mobile/DwarfMinerTest.java
@@ -11,10 +11,24 @@ import org.junit.Test;
 import contract.ElementType;
 import contract.Permeability;
 
+/**
+*The test DwarfMiner
+*
+*@author Group 4 A1 - Arras
+*/
+
 public class DwarfMinerTest extends DwarfMiner {
+/**
+*Create the dwarf
+*/
 	private DwarfMiner dwarf;
+	
+	
 	@Before
 	public void setUp() throws Exception {
+/**
+*Create a dwarf with position equal to (1;1)
+*/
 		this.dwarf = new DwarfMiner();
 		this.dwarf.setX(1);
 		this.dwarf.setY(1);
@@ -26,6 +40,10 @@ public class DwarfMinerTest extends DwarfMiner {
 
 	@Test
 	public void testGetNumberLife() {
+/**
+*Test if the dwarf number of life exists
+*Test if the number of life of the dwarf which are get are equal to the expected one (2)
+*/
 		assertNotNull(this.dwarf.getNumberLife());
 		int expected = 2;
 		assertEquals(expected, this.dwarf.getNumberLife());
@@ -33,6 +51,11 @@ public class DwarfMinerTest extends DwarfMiner {
 
 	@Test
 	public void testDwarfMiner() {
+/**
+*Test if the sprite of the dwarf exists
+*Test if the permeability of dwarf is BLOCKING
+*Test if the dwarf is a DwarfMiner
+*/
 		assertNotNull(this.dwarf.getSprite());
 		
 		Permeability expected1 = Permeability.BLOCKING;
