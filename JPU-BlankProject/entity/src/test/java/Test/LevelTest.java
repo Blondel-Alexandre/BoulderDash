190813e1a0b8561bbe07e762cf9f42470050bcbd
ThePikPik <44901108+ThePikPik@users.noreply.github.com>
2019-06-03 17:47:06 +0200

    Update LevelTest

--- JPU-BlankProject/entity/src/test/java/Test/LevelTest.java
+++ JPU-BlankProject/entity/src/test/java/Test/LevelTest.java
@@ -1,4 +1,4 @@
-/*package Test;
+package Test;
 
 import static org.junit.Assert.*;
 
@@ -10,13 +10,23 @@ import org.junit.Test;
 
 import entity.Level;
 
+/**
+*The test Level
+*
+*@author Group 4 A1 - Arras
+*/
 public class LevelTest extends Level {
-
+/**
+*Create a new map with a size of 30x30
+*/
 	protected int levelSize = 30;
 	char map[][] = new char[levelSize][levelSize];
 	private Level level;
 
-
+/** 
+*Test the initialization
+*/
+	
 	@Before
 	public void setUp() throws Exception {
 		this.level = new Level();
@@ -31,11 +41,17 @@ public class LevelTest extends Level {
 
 	@Test
 	public void testLevel() {
+/**
+*Test if the level is not null so if it exists
+*/
 		assertNotNull(this.level);
 	}
 
 	@Test
 	public void testLoadLevel() {
+/**
+*Test if there is a message or a level
+*/
 		this.level.loadLevel();
 		assertNotNull(this.level.getMessage());
 		assertNotNull(this.level.getLevel());
@@ -43,16 +59,25 @@ public class LevelTest extends Level {
 
 	@Test
 	public void testGetLevel() {
+/**
+*Test if the map exists
+*/
 		assertNotNull(this.map);
 	}
 
 	@Test
 	public void testGetId() {
+/**
+*Test if the level has an id
+*/
 		assertNotNull(this.level.getId());
 	}
 
 	@Test
 	public void testSetId() {
+/**
+*Test if the set id is equal to the id which is get, so if setId works
+*/
 		int id =10;
 		int test = 10;
 		this.level.setId(id);
@@ -61,18 +86,27 @@ public class LevelTest extends Level {
 
 	@Test
 	public void testGetKey() {
+/**
+*Test if the key exists
+*/
 		assertNotNull(this.level.getKey());
 		
 	}
 
 	@Test
 	public void testGetLevelSize() {
+/**
+*Test if the level size exists
+*/
 		assertNotNull(this.level.getLevelSize());
 		
 	}
 
 	@Test
 	public void testSetKey() {
+/**
+*Test if the set key is equal to the key which is get, so if setKey works
+*/
 		String key ="a";
 		String test = "a";
 		this.level.setKey(key);
@@ -81,12 +115,18 @@ public class LevelTest extends Level {
 
 	@Test
 	public void testGetMessage() {
+/**
+*Test if the message exists
+*/
 		assertNotNull(this.level.getMessage());
 		
 	}
 
 	@Test
 	public void testSetMessage() {
+/**
+*Test if the set message is equal to the message which is get, so if setMessage works
+*/
 		String message ="test";
 		String test = "test";
 		this.level.setMessage(message);
@@ -94,4 +134,4 @@ public class LevelTest extends Level {
 		
 	}
 
-}*/
+}
